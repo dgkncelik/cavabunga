6fb01b063e2c1a0053380194a823401deb951f1b
Dogukan CELIK <dogukan.celik@ebrandvalue.com>
2019-01-25 17:53:47 +0300

    initial commit

---
+++ src/main/java/xyz/dgkn/cavabunga/CavabungaApplication.java
@@ -0,0 +1,14 @@
+package xyz.dgkn.cavabunga;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CavabungaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CavabungaApplication.class, args);
+	}
+
+}
+
